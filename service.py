2e7d56703d01c20757bf470fb9be3c50658ceeb9
Sulaymonqori <ms13061312@gmail.com>
2023-12-31 17:48:09 +0500

    integrated with prettytable

--- service.py
+++ service.py
@@ -1,4 +1,5 @@
 from db import commit
+from prettytable import PrettyTable
 import db
 import utils
 import models
@@ -41,13 +42,18 @@ def create_todo_service(todo: models.Todo):
 
 
 def todo_list_service(user_id):
-    result = """NO.  ID     Todo                 Type       Completed\n"""
+    # result = """NO.  ID     Todo                 Type       Completed\n"""
+    # for i in range(len(todos)):
+    #     result += " {:<4} {:<5} {:<20} {:<10} {:<6}\n".format(i + 1, todos[i][0], todos[i][1], todos[i][2],
+    #                                                           ["No", "Yes"][todos[i][3]])
+    # return result
+
+    result = PrettyTable(["No", "ID", "Title", "Type", "Completed"])
     todos = db.get_todo_list(user_id)
     for i in range(len(todos)):
-        result += " {:<4} {:<5} {:<20} {:<10} {:<6}\n".format(i + 1, todos[i][0], todos[i][1], todos[i][2],
-                                                              ["No", "Yes"][todos[i][3]])
-    return result
+        result.add_row([i+1, todos[i][0], todos[i][1], todos[i][2], todos[i][3]])
 
+    return result
 
 def block_user_service(username):
     userdata = db.get_user_by_username(username)
